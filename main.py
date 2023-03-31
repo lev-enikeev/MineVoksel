a303a6756cb3595aaf95d21e300e4c96a3d45d71
lev-enikeev <l.yenikeev@gmail.com>
2023-03-31 20:21:24 +0300

    + новые баги

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from ursina.prefabs.first_person_controller import FirstPersonController
 from inventory import Inventory
 from quick_menu import QuickInventory
 from voxel import Voxel
-
+import time
 app = Ursina()
 
 inventory = None
@@ -16,12 +16,20 @@ def input(key):
             inventory = Inventory()
 
         player.enabled = False
+    if key == 'left mouse down':
+        hand.model ="model\hand_005"
 
 
     if key == 'r':
         destroy(inventory)
         player.enabled = True
 
+
+def update():
+    print(hand.model)
+    if "hand_005.obj" in hand.model:
+        print("tut")
+        hand.model ="model\hand_000"
 
 player = FirstPersonController()
 player.speed = 5
@@ -33,7 +41,7 @@ for x_dynamic in range(60):
     for z_dynamic in range(60):
         Voxel(player, position=(x_dynamic, 0, z_dynamic), texture=grass_texture)
 
-hand = Entity(parent=camera.ui, model='model/hand.obj',
+hand = Entity(parent=camera.ui, model='model/hand_000.obj',
               rotation=Vec3(150, -10, 0), position=Vec2(0.6, -0.6))
 
 
